09b1a3c1800e74001fd150b24b7d8e924460153d
seppe912 <39451979+seppe912@users.noreply.github.com>
2021-08-28 13:35:40 +0200

    Update broadlink-thermostat.py

--- data/broadlink-thermostat.py
+++ data/broadlink-thermostat.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!REPLACELBPBINDIR/venv/bin/python3
 # -*- coding: utf-8 -*
 from multiprocessing import Process, Lock, Pipe
 import multiprocessing, argparse, time, datetime, sys, os, signal, traceback, socket, sched, atexit
